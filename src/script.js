298b668a256e3f0f4dc34be3493c49b9f317b67b
PaulBallestra <paulballestra@yahoo.fr>
2023-03-01 17:57:33 +0100

    P1 Q3 Done :white_check_mark:

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 let stringReverse
 let stringPalindrome
+let stringMostCommon
 
 const validateQ1Button = document.querySelector('.btnQ1')
 const responseReverse = document.querySelector('.response-reverse')
 
 const validateQ2Button = document.querySelector('.btnQ2')
 const responsePalindrome = document.querySelector('.response-palindrome')
+
+const validateQ3Button = document.querySelector('.btnQ3')
+const responseMostCommon = document.querySelector('.response-mostcommon')
 
 //OnClick Q1
 validateQ1Button.addEventListener('click', () => {
@@ -17,6 +21,12 @@ validateQ1Button.addEventListener('click', () => {
 validateQ2Button.addEventListener('click', () => {
     stringPalindrome = document.getElementById('string-palindrome').value
     responsePalindrome.innerHTML = isPalindrome(stringPalindrome)
+})
+
+//OnClick Q3
+validateQ3Button.addEventListener('click', () => {
+    stringMostCommon = document.getElementById('string-mostcommon').value
+    responseMostCommon.innerHTML = printMostComChar(stringMostCommon)
 })
 
 //P1Q1 Function
@@ -36,4 +46,23 @@ function isPalindrome (strg){
         result = false
 
     return result
+}
+
+//P1Q3 Function
+function printMostComChar (strg){
+
+    var letterMostCommon
+    var letterNumber = 0
+
+    let numberOfLetters = [...strg].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {})
+
+    for(let letter in numberOfLetters){
+
+        if(numberOfLetters[letter] >= letterNumber){
+            letterMostCommon = letter
+            letterNumber = numberOfLetters[letter]
+        }
+    }
+
+    return letterMostCommon
 }
