d7d4e371817170de29140fb23a87c38a70326df4
PaulBallestra <paulballestra@yahoo.fr>
2023-03-01 17:20:51 +0100

    P1 Q2 Done :white_check_mark:

--- src/script.js
+++ src/script.js
@@ -1,19 +1,39 @@
 let stringReverse
+let stringPalindrome
 
-const validateQ1Button = document.querySelector('.btnValidate')
+const validateQ1Button = document.querySelector('.btnQ1')
 const responseReverse = document.querySelector('.response-reverse')
+
+const validateQ2Button = document.querySelector('.btnQ2')
+const responsePalindrome = document.querySelector('.response-palindrome')
 
 //OnClick Q1
 validateQ1Button.addEventListener('click', () => {
     stringReverse = document.getElementById('string-reverse').value
-    reverseString(stringReverse)
+    responseReverse.innerHTML = reverseString(stringReverse)
+})
+
+//OnClick Q2
+validateQ2Button.addEventListener('click', () => {
+    stringPalindrome = document.getElementById('string-palindrome').value
+    responsePalindrome.innerHTML = isPalindrome(stringPalindrome)
 })
 
 //P1Q1 Function
 function reverseString (strg){
+    return strg.split("").reverse().join("")
+}
 
-    var reverseStrg = strg.split("").reverse().join("")
+//P1Q2 Function
+function isPalindrome (strg){
 
-    responseReverse.innerHTML = reverseStrg
+    let reverse = reverseString(strg)
+    let result = false
 
+    if(strg == reverse)
+        result = true
+    else
+        result = false
+
+    return result
 }
